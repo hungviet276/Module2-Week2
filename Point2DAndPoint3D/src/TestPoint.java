542d8d5818650a060fdb12534f19f0855d37c644
Hung Viet <you@example.com>
2020-05-05 22:55:29 +0700

    add new file

---
+++ Point2DAndPoint3D/src/TestPoint.java
@@ -0,0 +1,8 @@
+public class TestPoint {
+    public static void main(String[] args) {
+        Point2D point2D = new Point2D(4.5f,5.5f);
+        System.out.println(point2D);
+        Point3D point3D = new Point3D(5.5f,6.5f,7.5f);
+        System.out.println(point3D);
+    }
+}
