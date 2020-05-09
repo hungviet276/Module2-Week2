9d5af02f70a40f38cc9599a5da58e3c037466ca3
hungviet276 <viethungkt7k6@gmail.com>
2020-05-09 08:49:55 +0700

    add new file

--- Book/src/Test.java
+++ Book/src/Test.java
@@ -1,36 +1,37 @@
 public class Test {
     public static void main(String[] args) {
-        ProgrammingBook pb1 = new ProgrammingBook("hoc cach hoc",250000,"python","abc");
-        ProgrammingBook pb2 = new ProgrammingBook("hoc cach choi",200000,"java","xyz");
-        ProgrammingBook pb3 = new ProgrammingBook("hoc cach ngu",257000,"java","nnn");
-        ProgrammingBook pb4 = new ProgrammingBook("hoc cach di",255000,"C","yyy");
-        ProgrammingBook pb5 = new ProgrammingBook("hoc cach an",210000,"java","zzz");
+        ProgrammingBook programmingBook1 = new ProgrammingBook("hoc cach hoc", 250000, "python", "Laravel");
+        ProgrammingBook programmingBook2 = new ProgrammingBook("hoc cach choi", 200000, "java", "Ruby on Rails");
+        ProgrammingBook programmingBook3 = new ProgrammingBook("hoc cach ngu", 257000, "java", "Spring");
+        ProgrammingBook programmingBook4 = new ProgrammingBook("hoc cach di", 255000, "C", "Bootstrap");
+        ProgrammingBook programmingBook5 = new ProgrammingBook("hoc cach an", 210000, "java", "Angular JS");
 
-        FictionBook fb1 = new FictionBook("tim cach ngu",236000,"dfdf");
-        FictionBook fb2 = new FictionBook("tim cach an",242000,"rgr");
-        FictionBook fb3 = new FictionBook("tim cach choi",210000,"dfgg");
-        FictionBook fb4 = new FictionBook("tim cach di",102000,"jmghj");
-        FictionBook fb5 = new FictionBook("tim cach bay",111000,"err");
-       // System.out.println(fb1 instanceof FictionBook);
+        FictionBook fictionBook1 = new FictionBook("tim cach ngu", 236000, "travel");
+        FictionBook fictionBook2 = new FictionBook("tim cach an", 242000, "life");
+        FictionBook fictionBook3 = new FictionBook("tim cach choi", 210000, "family");
+        FictionBook fictionBook4 = new FictionBook("tim cach di", 102000, "friend");
+        FictionBook fictionBook5 = new FictionBook("tim cach bay", 111000, "bar");
+
         //Tinh tong gia tien
-    Book[] books = {pb1,pb2,pb3,pb4,pb5,fb1,fb2,fb3,fb4,fb5};
-    double price=0;
-    for (int i =0; i < books.length;i++){
-        price += books[i].getPrice();
-    }
-        System.out.println(price);
-        //dem so programming book co language laf java
-        int count =0;
-        for (int j =0; j < books.length;j++){
-            if (books[j] instanceof ProgrammingBook){
-                if (((ProgrammingBook)books[j]).getLanguage().equals("java")){
-                    count++;
+            Book[] books = {programmingBook1, programmingBook2, programmingBook3, programmingBook4, programmingBook5, fictionBook1, fictionBook2, fictionBook3, fictionBook4, fictionBook5};
+            double totalPriceOfBooks = 0;
+            for (int i = 0; i < books.length; i++) {
+                totalPriceOfBooks += books[i].getPrice();
+            }
+            System.out.println("Total price books: "+totalPriceOfBooks);
+
+        //dem so programming book co language la java
+        int countBooks = 0;
+        for (int j = 0; j < books.length; j++) {
+            if (books[j] instanceof ProgrammingBook) {
+                if (((ProgrammingBook) books[j]).getLanguage().equals("java")) {
+                    countBooks++;
                 }
 
             }
         }
-        System.out.println(count);
-        
+        System.out.println("So sach Java la "+countBooks);
+
 
     }
 }
